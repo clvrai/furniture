fix demo recording (ob off by one)

env/furniture.py
@@ -213,12 +213,14 @@ class FurnitureEnv(metaclass=EnvMeta):
             action = {key: val for ac_i in action for key, val in ac_i.items()}
         if isinstance(action, dict):
             action = np.concatenate([action[key] for key in self.action_space.shape.keys()])
+        if self._record_demo:
+            self._demo.add(ob=ob)
         ob, reward, done, info = self._step(action)
         done, info, penalty = self._after_step(reward, done, info)
         reward += penalty
         if self._record_demo:
             self._store_qpos()
-            self._demo.add(ob=ob, action=action, reward=reward)
+            self._demo.add(action=action, reward=reward)
         return ob, reward, done, info
 
     def _before_step(self):