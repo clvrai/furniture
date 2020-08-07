689d6c821c4e6f29a769d0d3c9d865ae7428b2fc
Youngwoon Lee <lywoon89@gmail.com>
2020-08-07 02:16:14 -0700

    unstablize ik control for IL with joint space control

--- env/furniture.py
+++ env/furniture.py
@@ -2703,10 +2703,10 @@ class FurnitureEnv(metaclass=EnvMeta):
         Take multiple physics simulation steps, bounded by self._control_timestep
         """
         # stop moving arm for IK control, critical for BC with IK
-        for arm in self._arms:
-            for qvel_addr in self._ref_joint_vel_indexes[arm]:
-                self.sim.data.qvel[qvel_addr] = 0.0
-        self.sim.forward()
+        # for arm in self._arms:
+        #     for qvel_addr in self._ref_joint_vel_indexes[arm]:
+        #         self.sim.data.qvel[qvel_addr] = 0.0
+        # self.sim.forward()
 
         connect = action[-1]
 
